758353348a83b594162b90d854537b513fc97ad2
cchuong <cchuong@sfu.ca>
2023-04-09 21:59:34 -0700

    Update plot.mars.R

--- mars/R/plot.mars.R
+++ mars/R/plot.mars.R
@@ -1,20 +1,28 @@
-plot.mars<-function(mars,...){
-  par(mfrow=c(2,2))
-
-  # CDF Plot
-  cdf=ecdf(x=abs(mars$residuals))
-  plot(cdf,do.points=F,xlab="abs(Residuals)",ylab = "Proportion",main="Cumulative Distribution")
-  curve(cdf,from=0,to=max(abs(mars$residuals)),add=T)
-
-  # Residual vs Fitted Plot
+plot.mars<-function(mars,which=c(1,2,3,4),col="purple",numpoints=3,...){
+  par(ask=T)
   class(mars) = class(mars)[2]; #get lm class
-  plot(mars,which=1)
 
-  # Q-Q Plot
-  plot(mars,which=2)
+  if (1 %in% which){
+    # CDF Plot
+    cdf=ecdf(x=abs(mars$residuals))
+    plot(cdf,do.points=F,xlab="abs(Residuals)",ylab = "Proportion",main="Cumulative Distribution")
+    curve(cdf,from=0,to=max(abs(mars$residuals)),add=T,col=col)
+  }
 
-  # Scale - Location Plot
-  plot(mars,which=3)
+  if (2 %in% which){
+    # Residual vs Fitted Plot
+    plot(mars,which=1,col=col,id.n=numpoints)
+  }
+
+  if (3 %in% which){
+    # Q-Q Plot
+    plot(mars,which=2,col=col,id.n=numpoints)
+  }
+
+  if (4 %in% which){
+    # Scale - Location Plot
+    plot(mars,which=3,col=col,id.n=numpoints)
+  }
 
   class(mars) = c("mars",class(mars))
 }
