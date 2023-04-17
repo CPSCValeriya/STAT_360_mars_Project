f6516a34ad8baf9ba2400a57c73e04ae9555ac42
Valeriya <vkistrin@sfu.ca>
2023-04-16 22:02:15 -0700

    Remove redundant files

--- mars/tests/testthat/testLOF.R
+++
@@ -1,6 +0,0 @@
-load("testLOF.RData")
-load("testmc.RData")
-load("testfwd_stepwise.RData")
-data = data.frame(y=testfwd$y,testfwd$B)
-test_that("testing LOF", {
-  expect_equal(LOF(y~.-1, data, testmc), testLOF)} )
--- mars/tests/testthat/testmars.R
+++
@@ -1,5 +0,0 @@
-load("testmars.RData")
-test_that("mars() returns the correct object", {
-  expect_equal(mars(y~.,data=marstestdata,control=testmc),
-               testmars, ignore_attr=TRUE)
-})
